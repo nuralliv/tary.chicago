9389d603173b3d3a60287779dd8f0d274be0995e
Nurali <izetkhannurali@gmail.com>
2025-01-02 18:17:42 +0500

    special menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,13 +1,17 @@
 import './Menu.css'
 import backOu from '../../assets/img/component-menu/image 32.svg'
-import mealMenu from '../../assets/img/component-menu/meal-menu.png'
-import drinkMenu from '../../assets/img/component-menu/drink-menu.png'
+import mealMenu1 from '../../assets/img/component-menu/special-menu1.png'
+import mealMenu2 from '../../assets/img/component-menu/special-menu2.png'
+import mealMenu3 from '../../assets/img/component-menu/special-menu3.png'
+import mealMenu4 from '../../assets/img/component-menu/special-menu4.png'
+
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 const Menu = () => {
+
    useEffect(() => {
       AOS.init({
          duration: 1200, // Длительность анимации в миллисекундах
@@ -15,30 +19,75 @@ const Menu = () => {
    }, []);
    return (
       <section className='menu'>
-         <h2 data-aos="fade-up">Menu</h2>
-         <p data-aos="fade-up" data-aos-delay="200">Explore our menu, featuring a blend of traditional and modern favorites inspired by Kazakh culture. From rich coffees to homemade baked goods and hearty meals, there&apos;s something for everyone to enjoy.</p>
-
+         <div className="menu-comp-top">
+            <h2 data-aos="fade-up">Special Menu</h2>
+            <p className='menu-tit' data-aos="fade-up" data-aos-delay="200">
+               Discover the heart of our culinary artistry with our carefully curated Special Menu, designed to delight your senses and elevate your dining experience.            </p>
+            
+            <button className='menu-component-btn'>
+               <Link to={'/tary.chicago/menu'} className='to-menu-page'>
+                  view menu
+               </Link>
+            </button>
+         </div>
          <div className="back-uzor">
+
+            <div className="back-rec"></div>
             <div className="uzor-images">
                <img src={backOu} alt="" />
                <img src={backOu} id='ou-second' alt="" />
                <img src={backOu} id='ou-third' alt="" />
 
             </div>
-            <div className="back-rec"></div>
          </div>
-         <div className="menus" data-aos="fade-up" data-aos-delay="400">
-            <div className="menu-min">
-               <img src={mealMenu} alt="" />
-               <div className="ver-line"></div>
-               <button><Link className='men-nav' to={'/tary.chicago/menu'}>Main menu</Link></button>
+         <div className="menus" >
+            <div data-aos="fade-down" data-aos-delay="400" className="special-meal" style={{
+               backgroundImage: `url(${mealMenu1})`,
+               marginTop: '100px',
+            }}>
+               <div className="meal-dark">
+                  <div className="meal-content">
+                     <h4 className="cost">$11.00</h4>
+                     <h3 className='meal-name'>Tary Coffee – A Sip of Perfection</h3>
+                     <p>Tary Coffee isn’t just a drink—it’s a moment of joy, crafted to bring people together. Inspired by the warmth and energy of a bustling mela (festival), each cup embodies the spirit of connection, celebration, and tradition.</p>
+                  </div>
+               </div>
             </div>
-            <div className='menu-min'
-            ><img src={drinkMenu} alt="" />
-               <div className="ver-line"></div>
-               <button><Link className='men-nav' to={'/tary.chicago/menu'}>Tea & Coffee</Link></button>
+            <div data-aos="fade-up" data-aos-delay="400" className="special-meal" style={{
+               backgroundImage: `url(${mealMenu2})`
+            }}>
+               <div className="meal-dark">
+                  <div className="meal-content">
+                     <h4 className="cost">$11.00</h4>
+                     <h3 className='meal-name'>Tary Coffee – A Sip of Perfection</h3>
+                     <p>Tary Coffee isn’t just a drink—it’s a moment of joy, crafted to bring people together. Inspired by the warmth and energy of a bustling mela (festival), each cup embodies the spirit of connection, celebration, and tradition.</p>
+                  </div>
+               </div>
             </div>
+            <div data-aos="fade-down" data-aos-delay="400" className="special-meal" style={{
+               backgroundImage: `url(${mealMenu3})`,
+               marginTop: '100px',
 
+            }}>
+               <div className="meal-dark">
+                  <div className="meal-content">
+                     <h4 className="cost">$11.00</h4>
+                     <h3 className='meal-name'>Tary Coffee – A Sip of Perfection</h3>
+                     <p>Tary Coffee isn’t just a drink—it’s a moment of joy, crafted to bring people together. Inspired by the warmth and energy of a bustling mela (festival), each cup embodies the spirit of connection, celebration, and tradition.</p>
+                  </div>
+               </div>
+            </div>
+            <div data-aos="fade-up" data-aos-delay="400" className="special-meal" style={{
+               backgroundImage: `url(${mealMenu4})`
+            }}>
+               <div className="meal-dark">
+                  <div className="meal-content">
+                     <h4 className="cost">$11.00</h4>
+                     <h3 className='meal-name'>Tary Coffee – A Sip of Perfection</h3>
+                     <p>Tary Coffee isn’t just a drink—it’s a moment of joy, crafted to bring people together. Inspired by the warmth and energy of a bustling mela (festival), each cup embodies the spirit of connection, celebration, and tradition.</p>
+                  </div>
+               </div>
+            </div>
          </div>
       </section>
    )
